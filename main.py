c87c6eb00b12412ad4a2c16afd5b24d4f07b986f
Armando Ayala <armando@Armandos-MacBook-Air.local>
2023-11-21 20:00:27 -0600

    Creating 1D, 2D and 3D arrays with numpy

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import numpy as np
 
-matriz = np.array([1,2,3,4,5])
-
+matriz = np.array([1, 2, 3, 4, 5])
 print(matriz)
-print("Hello world")
 
+m2d = np.array([[1, 2, 3], [4, 5, 6]])
+print(m2d)
+
+lista = [1, 2, 3]
+lista1d = np.array(lista)
+print(lista1d)
+
+lista3d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matriz3d = np.array(lista3d)
+print(matriz3d)
+
+#TO CREATE MATRIZ WITH CONSICUTIVE NUMBER AND DIFFERENT SHAPE
+m = np.arange(15).reshape(3,5)
+print(m)
